a125bdb15d5909b206f2013f83f52a71195a53a2
castle <castleliu40@gmail.com>
2018-04-26 14:41:43 +0800

    add body process to simple tag hanlder

--- customTag/src/com/example/tag/SimpleTagTest.java
+++ customTag/src/com/example/tag/SimpleTagTest.java
@@ -5,6 +5,7 @@ import javax.servlet.jsp.JspException;
 
 public class SimpleTagTest extends SimpleTagSupport {
     public void doTag() throws JspException,IOException{
-        getJspContext().getOut().print("This is the lamest use of a custom tag");
+        // getJspContext().getOut().print("This is the lamest use of a custom tag");
+        getJspBody().invoke(null);
     }
 }
--- customTag/web/simpleTag.jsp
+++ customTag/web/simpleTag.jsp
@@ -1,4 +1,6 @@
 <%@ taglib prefix="myTags" uri="simpleTags" %>
 <html><body>
-        <myTags:simple1/>
+        <myTags:simple1>
+            This is the body.
+        </myTags:simple1>
 </body></html>
